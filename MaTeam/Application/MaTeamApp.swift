12b6fc77c98f956b1a50d9d7fb003ab8ae997da1
Elyes <lyesderouich@gmail.com>
2023-01-05 08:31:56 +0100

    Initial Commit

---
+++ MaTeam/Application/MaTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaTeamApp.swift
+//  MaTeam
+//
+//  Created by Derouiche Elyes on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MaTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaTeamUITests/MaTeamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MaTeamUITestsLaunchTests.swift
+//  MaTeamUITests
+//
+//  Created by Derouiche Elyes on 05/01/2023.
+//
+
+import XCTest
+
+final class MaTeamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
